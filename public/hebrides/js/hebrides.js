4f945c766082f85a7813cb549d3de8522924b231
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-10-18 17:34:24 -0400

    Add in firebase push from main hebrides file

--- public/hebrides/js/hebrides.js
+++ public/hebrides/js/hebrides.js
@@ -107,7 +107,8 @@ $(document).ready(function() {
     // pitchShift.connect(audioCtx.destination);
 
 
-    var initialTempo = $('#bpm').text();
+    var initialTempo = 1 * $('#bpm').text();
+    firebase.database().ref("tempo").set(initialTempo);
     var tempo = initialTempo,
         conducted = 0,
         beats = [];
@@ -117,7 +118,8 @@ $(document).ready(function() {
       if (conducted == 0) {
         conducted = 1;
         var msPerBeat = (60 * 1000 / initialTempo);
-        beats = [timeStamp-(msPerBeat*5),timeStamp-(msPerBeat*4),timeStamp-(msPerBeat*3),timeStamp-(msPerBeat*2),timeStamp-msPerBeat,timeStamp];
+        beats = [timeStamp-(msPerBeat*6),timeStamp-(msPerBeat*5),timeStamp-(msPerBeat*4),timeStamp-(msPerBeat*3),timeStamp-(msPerBeat*2),timeStamp-msPerBeat,timeStamp];
+        firebase.database().ref("tempo").set(initialTempo);
 
         youtubePlayer.pauseVideo();
         clearInterval(audioMonitor);
@@ -133,61 +135,20 @@ $(document).ready(function() {
         }, 166);
 
       } else {
-        if (beats.length == 6) {
-          beats.push(timeStamp);
-        } else {
-          beats.shift();
-          beats.push(timeStamp);
-
-          tempo = 60 * 1000 / ((
-            (beats[1] - beats[0] + beats[2] - beats[1]) +
-            (beats[3] - beats[2] + beats[4] - beats[3]) * 2 +
-            (beats[5] - beats[4] + beats[6] - beats[5]) * 4) / 14);
-
-          beats.shift();
-
-          $('#bpm').text(tempo);
-          audioNode.mediaElement.playbackRate = tempo / initialTempo;
-          console.log(tempo);
-          console.log(audioNode.mediaElement.playbackRate);
-        }
+        beats.shift();
+        beats.push(timeStamp);
+        tempo = 60 * 1000 / ((
+          (beats[1] - beats[0] + beats[2] - beats[1]) +
+          (beats[3] - beats[2] + beats[4] - beats[3]) * 2 +
+          (beats[5] - beats[4] + beats[6] - beats[5]) * 4) / 14);
+        $('#bpm').text(tempo);
+        audioNode.mediaElement.playbackRate = tempo / initialTempo;
+        firebase.database().ref("tempo").set(tempo);
       }
     }
 
     $('#bpm-tap').click(function(e) {
       processBeat(Date.now());
     });
-
-    if ('ondeviceorientation' in window) {
-      window.addEventListener('deviceorientation', function(event) {
-         $('#beta').text(Math.round(event.beta));
-         $('#gamma').text(Math.round(event.gamma));
-         $('#alpha').text(Math.round(event.alpha));
-      });
-    }
-
-    if ('ondevicemotion' in window) {
-      var eventCounter = 0;
-      var beatTimeStamp = 0;
-      var beatCounter = 0;
-      var accelerationSensitivity = 8;
-      var timeSensitivity = 500;
-      window.addEventListener('devicemotion', function(event) {
-        eventCounter++;
-        var x = Math.round(event.acceleration.x);
-        var y = Math.round(event.acceleration.y);
-        var z = Math.round(event.acceleration.z);
-        if ((x > accelerationSensitivity || y > accelerationSensitivity || z > accelerationSensitivity) && ((event.timeStamp - beatTimeStamp) > timeSensitivity) ) {
-          beatTimeStamp = event.timeStamp;
-          beatCounter > 3 ? beatCounter = 1 : beatCounter++;
-          console.log(beatCounter + '! At time: ' + beatTimeStamp);
-          processBeat(beatTimeStamp);
-        }
-        //console.log(event.acceleration.x,event.acceleration.y,event.acceleration.z);
-        $('#acceleration-x').text(Math.round(event.acceleration.x));
-        $('#acceleration-y').text(Math.round(event.acceleration.y));
-        $('#acceleration-z').text(Math.round(event.acceleration.z));
-      });
-    }
   }
 });
