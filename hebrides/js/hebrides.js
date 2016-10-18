b7d1a49d55268d4ed4753e8d6680a1cc6fc46b32
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-10-18 12:36:15 -0400

    First pass at motion sensing beat detection

--- hebrides/js/hebrides.js
+++ hebrides/js/hebrides.js
@@ -24,3 +24,34 @@ $(document).ready(function() {
     event.target.playVideo();
   }
 
+  if ('ondeviceorientation' in window) {
+    console.log('yes ondeviceorientation');
+    window.addEventListener('deviceorientation', function(event) {
+       $('#beta').text(Math.round(event.beta));
+       $('#gamma').text(Math.round(event.gamma));
+       $('#alpha').text(Math.round(event.alpha));
+    });
+  }
+
+  if ('ondevicemotion' in window) {
+    console.log('yes ondevicemotion');
+    var eventCounter = 0;
+    var beatTimeStamp = 0;
+    window.addEventListener('devicemotion', function(event) {
+      eventCounter++;
+      var x = Math.round(event.acceleration.x);
+      var y = Math.round(event.acceleration.y);
+      var z = Math.round(event.acceleration.z);
+      if (x > 10 || y > 10 || z > 10 && (event.timeStamp - beatTimeStamp) > 200 ) {
+        beatTimeStamp = event.timeStamp;
+        console.log('Beat! At time: ' + beatTimeStamp);
+        console.log(event);
+        console.log(x,y,z);
+      }
+      //console.log(event.acceleration.x,event.acceleration.y,event.acceleration.z);
+      $('#acceleration-x').text(Math.round(event.acceleration.x));
+      $('#acceleration-y').text(Math.round(event.acceleration.y));
+      $('#acceleration-z').text(Math.round(event.acceleration.z));
+    });
+  }
+});
