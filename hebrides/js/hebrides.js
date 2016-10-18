e09cbf2e1a41f605a9505d6e3eb503721d656bb5
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-10-18 12:53:42 -0400

    Count the beat in the console!

--- hebrides/js/hebrides.js
+++ hebrides/js/hebrides.js
@@ -25,7 +25,6 @@ $(document).ready(function() {
   }
 
   if ('ondeviceorientation' in window) {
-    console.log('yes ondeviceorientation');
     window.addEventListener('deviceorientation', function(event) {
        $('#beta').text(Math.round(event.beta));
        $('#gamma').text(Math.round(event.gamma));
@@ -34,19 +33,20 @@ $(document).ready(function() {
   }
 
   if ('ondevicemotion' in window) {
-    console.log('yes ondevicemotion');
     var eventCounter = 0;
     var beatTimeStamp = 0;
+    var beatCounter = 0;
+    var accelerationSensitivity = 8;
+    var timeSensitivity = 500;
     window.addEventListener('devicemotion', function(event) {
       eventCounter++;
       var x = Math.round(event.acceleration.x);
       var y = Math.round(event.acceleration.y);
       var z = Math.round(event.acceleration.z);
-      if (x > 10 || y > 10 || z > 10 && (event.timeStamp - beatTimeStamp) > 200 ) {
+      if ((x > accelerationSensitivity || y > accelerationSensitivity || z > accelerationSensitivity) && ((event.timeStamp - beatTimeStamp) > timeSensitivity) ) {
         beatTimeStamp = event.timeStamp;
-        console.log('Beat! At time: ' + beatTimeStamp);
-        console.log(event);
-        console.log(x,y,z);
+        beatCounter > 3 ? beatCounter = 1 : beatCounter++;
+        console.log(beatCounter + '! At time: ' + beatTimeStamp);
       }
       //console.log(event.acceleration.x,event.acceleration.y,event.acceleration.z);
       $('#acceleration-x').text(Math.round(event.acceleration.x));
