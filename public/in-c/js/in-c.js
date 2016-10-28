1a953883dae3d299fc336223902547e6c7a5353b
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-10-28 00:38:45 -0400

    We've got a metronome...huzzah

--- public/in-c/js/in-c.js
+++ public/in-c/js/in-c.js
@@ -2,39 +2,33 @@
 //var PitchShift = require('soundbank-pitch-shift');
 
 $(document).ready(function() {
-  var inC = {
-    init : function() {
+  var inC = function() {
+    var init = function() {
+      var note = 72; // the MIDI note
+      var velocity = 100; // how hard the note hits
+      // play the note
+      MIDI.setVolume(0, 100);
+      MIDI.setVolume(1, 80);
+      MIDI.setVolume(2, 60);
+      MIDI.programChange(0, MIDI.GM.byName["guitar_harmonics"].number);
       MIDI.programChange(1, MIDI.GM.byName["marimba"].number);
       MIDI.programChange(2, MIDI.GM.byName["baritone_sax"].number);
-      var channel = 1; // the MIDI note
-      var note = 50; // the MIDI note
-      var velocity = 127; // how hard the note hits
-      // play the note
-      MIDI.setVolume(0, 127);
-      MIDI.noteOn(channel, note, velocity, .5);
-      MIDI.noteOff(channel, note, 1.5);
-      MIDI.noteOn(channel, note + 2, velocity, 1);
-      MIDI.noteOff(channel, note + 2, 2);
-      MIDI.noteOn(channel, note + 4, velocity, 1.5);
-      MIDI.noteOff(channel, note + 4, 2.5);
-      MIDI.noteOn(0, note, velocity, 5);
-      MIDI.noteOff(0, note, 5.5);
-      MIDI.noteOn(0, note + 2, velocity, 6);
-      MIDI.noteOff(0, note + 2, 7);
-      MIDI.noteOn(0, note + 4, velocity, 8);
-      MIDI.noteOff(0, note + 4, 8);
-      MIDI.noteOn(2, note, velocity, 1);
-      MIDI.noteOff(2, note, 3);
-      MIDI.noteOn(2, note + 2, velocity, 3);
-      MIDI.noteOff(2, note + 2, 7);
-      MIDI.noteOn(2, note + 4, velocity, 7);
-      MIDI.noteOff(2, note + 4, 12);
-    }
+
+      //MIDI.noteOn(channel, note, velocity, delay);
+
+      var start = new Date().getTime()
+      var metronome = setInterval(function() {
+        var time = new Date().getTime() - start;
+        console.log(time);
+        MIDI.noteOn(1, 72, 100, 0);
+        MIDI.noteOff(1, 72, 0.25);
+      }, 272);
+    };
   };
 
   	MIDI.loadPlugin({
   		soundfontUrl: "./audio/",
-  		instruments: [ "acoustic_grand_piano", "marimba", "baritone_sax" ],
+  		instruments: [ "guitar_harmonics", "marimba", "baritone_sax" ],
   		onprogress: function(state, progress) {
   			console.log(state, progress);
   		},
