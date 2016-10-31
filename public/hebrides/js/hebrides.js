408be15cfcd1acdbfdafbf4a7f443340ddc6594d
Nick Brown <nicholas.tyler.brown@gmail.com>
2016-10-30 22:32:21 -0400

    Add score PDF, integrate PDF.js for rendering and page turning

--- public/hebrides/js/hebrides.js
+++ public/hebrides/js/hebrides.js
@@ -168,5 +168,65 @@ $(document).ready(function() {
       }
     });
 
+    var scoreURL = './images/full-score.pdf';
+    PDFJS.disableWorker = true;
+    var pdfDoc = null,
+      pageNum = 3,
+      pageRendering = false,
+      pageNumPending = null,
+      scale = 1.6,
+      canvas = document.getElementById('score-canvas'),
+      ctx = canvas.getContext('2d');
+
+    function renderPage(num) {
+      pageRendering = true;
+      pdfDoc.getPage(num).then(function(page) {
+        var viewport = page.getViewport(scale);
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        var renderContext = {
+          canvasContext: ctx,
+          viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.promise.then(function () {
+          pageRendering = false;
+          if (pageNumPending !== null) {
+            renderPage(pageNumPending);
+            pageNumPending = null;
+          }
+        });
+      });
+    }
+    function queueRenderPage(num) {
+      if (pageRendering) {
+        pageNumPending = num;
+      } else {
+        renderPage(num);
+      }
+    }
+    function previousPage() {
+      if (pageNum <= 1) {
+        return;
+      }
+      pageNum--;
+      queueRenderPage(pageNum);
+    }
+    function nextPage() {
+      if (pageNum >= pdfDoc.numPages) {
+        return;
+      }
+      pageNum++;
+      queueRenderPage(pageNum);
+    }
+
+    $('.previous-page').click(previousPage);
+    $('.next-page').click(nextPage);
+
+    PDFJS.getDocument(scoreURL).then(function (pdfDoc_) {
+      pdfDoc = pdfDoc_;
+      renderPage(pageNum);
+    });
+
   }
 });
